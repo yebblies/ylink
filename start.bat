d68341866e1e679ee2c88ca662566c496ae6037b
yebblies <yebblies@gmail.com>
2012-10-13 03:23:21 +1100

    Add dmc path to start, and don't assume F drive

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 set INCLUDE=%INCLUDE%;C:\D\dmd2\windows\inc;
-set LINK=%LINK%;C:\D\dmd2\windows\lib;F:\Documents\Desktop\D\Lib;
-set PATH=%PATH%;F:\Documents\Desktop\D\Bin;
+set LINK=%LINK%;C:\D\dmd2\windows\lib;\Documents\Desktop\D\Lib;C:\D\dm\lib
+set PATH=%PATH%;\Documents\Desktop\D\Bin;
 cmd
